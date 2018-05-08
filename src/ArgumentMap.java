458b9f7c8244fef795c4feb1631e19b7ea35108f
GabrielCee27 <gabrielcee81@gmail.com>
2018-05-07 18:21:37 -0700

    Added 'limit' as a flag.

--- src/ArgumentMap.java
+++ src/ArgumentMap.java
@@ -77,7 +77,7 @@ public class ArgumentMap {
 		
 		if(arg.equalsIgnoreCase("-index") || arg.equalsIgnoreCase("-path") || arg.equalsIgnoreCase("-url") 
 				|| arg.equalsIgnoreCase("-query") || arg.equalsIgnoreCase("-exact") || arg.equalsIgnoreCase("-results")
-				|| arg.equalsIgnoreCase("-threads") || arg.equalsIgnoreCase("-url") ) {
+				|| arg.equalsIgnoreCase("-threads") || arg.equalsIgnoreCase("-url") || arg.equalsIgnoreCase("-limit") ) {
 			return true;
 		}
 		
